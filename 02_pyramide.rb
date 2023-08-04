a2720c03a3b3a865cee9f2d47fbf8d9520d287f1
fadhima <fadhimatazahra.ndiaye@uadb.edu.sn>
2023-08-04 17:11:13 +0000

    commit

---
+++ 02_pyramide.rb
@@ -0,0 +1,20 @@
+def wtf_pyramid
+    puts "choisis un nombre paire"
+    print ">"
+    number = gets.chomp.to_i
+    x = number/2
+if number % 2 != 0
+    
+    for i in 1..number 
+             if i%2 != 0
+                    print "#"*i
+                    puts ""
+            
+    
+    end
+
+end
+
+end
+wtf_pyramid()
+     
