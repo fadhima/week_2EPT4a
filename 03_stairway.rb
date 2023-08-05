afa48f2387c8047840e51da59855ebcd9501ec63
fadhima <fadhimatazahra.ndiaye@uadb.edu.sn>
2023-08-05 14:14:19 +0000

    jeux

---
+++ 03_stairway.rb
@@ -0,0 +1,17 @@
+scalier = 0
+while scalier < 10
+    i = rand(1..6)
+    if i == 5 || i == 6
+        scalier+=1
+puts "tu es a la marche #{scalier}"
+    elsif   i == 1
+        scalier -= 1
+        puts "tu descend d'un marche #{scalier}" 
+    else i == 2 || i == 3 || i == 3
+        puts "tu ne bouge pas #{scalier}"
+    end
+
+end
+puts "*****************"
+puts "***Bravo***"
+puts "Le jeu est termine"
